df8f7deeadcf2ff3973ea23edd9a4631eb43a88e
bogdanGR <fant1k450@gmail.com>
2024-04-28 10:59:23 +0300

    add projects component, render it in projects page with dummy data

---
+++ src/components/ProjectsComponent.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="card text-start mb-3">
+    <div class="card-body">
+      <div class="row">
+        <div class="col-md-2">
+          Framework
+        </div>
+        <div class="col-md-10">
+          {{ framework }}
+        </div>
+          <div class="col-md-2">
+            Project Name
+          </div>
+          <div class="col-md-10">
+            {{ title }}
+          </div>
+          <div class="col-md-2">
+            Full Project Name
+          </div>
+          <div class="col-md-10">
+            {{ fullProjectName }}
+          </div>
+        <div class="col-md-2">
+          Participants
+        </div>
+        <div class="col-md-10">
+          {{ participants }}
+        </div>
+          <div class="col-md-2">
+            Budget
+          </div>
+          <div class="col-md-10">
+            {{ budget }}
+          </div>
+          <div class="col-md-2">
+            Duration
+          </div>
+          <div class="col-md-10">
+            {{ duration }}
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'category_title', 'framework', 'fullProjectName', 'participants', 'budget', 'duration']
+}
+</script>
+<style scoped>
+
+</style>
--- src/pages/ProjectsPage.vue
+++ src/pages/ProjectsPage.vue
@@ -1,7 +1,52 @@
 <template>
-  <h1>Projects</h1>
+  <h1 class="mt-2">Projects</h1>
+  <div class="container">
+    <h2 class="text-start">European In Progress</h2>
+    <projects-component
+        v-for="project in projects"
+        :key="project.id"
+        :framework="project.framework"
+        :title="project.projectName"
+        :fullProjectName="project.fullProjectName"
+        :participants="project.participants"
+        :budget="project.budget"
+        :duration="project.duration"
+    ></projects-component>
+  </div>
 </template>
 
+<script>
+import ProjectsComponent from "@/components/ProjectsComponent.vue";
+export default {
+  components: {
+    ProjectsComponent
+  },
+  data() {
+    return {
+      projects: [
+        {
+            id: 1,
+            framework: 'Framework	H2020',
+            projectName: 'iRead (731724)',
+            fullProjectName: 'Infrastructure and integrated tools for personalized learning of reading skill',
+            participants: 'NATIONAL TECHNICAL UNIVERSITY OF ATHENS, DOLPHIN COMPUTER ACCESS, FISH IN A BOTTLE, KNOWBLE, GOETEBORGS UNIVERSITET, PANEPISTIMIO IOANNINON',
+            budget: '€ 5547042,50',
+            duration: '2017-2020 (48 months)',
+        },
+        {
+          id: 1,
+          framework: 'Framework erasmus+',
+          projectName: 'eCrisis',
+          fullProjectName: 'Europe in Crisis',
+          participants: 'http://ecrisis.eu/partners',
+          budget: '€ 555569,50',
+          duration: '2016-2018 (36 months)',
+        }
+      ]
+    }
+  },
+}
+</script>
 <style scoped>
 
 </style>
