07c3cb882aac8a480f2cb96c9390c036e364243b
bogdanGR <fant1k450@gmail.com>
2024-05-07 21:12:34 +0300

    import fontawsome icons, add icons to about page

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -8,15 +8,15 @@
         <h3 class="mr-auto"><strong>{{ $t('Address') }}</strong></h3>
         <p class="mb-0">
           <strong>
-            <a href="https://maps.app.goo.gl/TwLzsnJdhEmD8x518" target="_blank">{{ $t('Ag. Spyridonos Str., Egaleo, Postal Code 12243, Athens') }}</a>
+            <a href="https://maps.app.goo.gl/TwLzsnJdhEmD8x518" target="_blank"><i class="fa-regular fa-map" style="color: red"></i> {{ $t('Ag. Spyridonos Str., Egaleo, Postal Code 12243, Athens') }}</a>
           </strong>
         </p>
-        <a class="d-block" href="emailto: ice@uniwa.gr">ice@uniwa.gr</a>
-        <a class="d-block" href="tel:2105385312">TEL +30.210-538-5312</a>
-        <a class="d-block" href="tel:2105910975">FAX: +30.210-591-0975</a>
+        <a class="d-block" href="emailto: ice@uniwa.gr"><i class="fa-solid fa-envelope" style="color: #1a037e"></i> ice@uniwa.gr</a>
+        <a class="d-block" href="tel:2105385312"><i class="fa-solid fa-phone"  style="color: #1a037e"></i> +30.210-538-5312</a>
+        <a class="d-block" href="tel:2105910975"><i class="fa-solid fa-fax"  style="color: #1a037e"></i> +30.210-591-0975</a>
 
-        <h3 class="mr-auto"><strong>{{ $t('Map') }}</strong></h3>
-        <a href="https://maps.app.goo.gl/DjqWjd8sXgLmm8sd8" target="_blank">{{ $t('Campus on Google maps') }}</a>
+        <h3 class="mr-auto"><strong>{{ $t('Map') }}<i class="fa-solid fa-location-dot m-lg-2" style="color: red"></i></strong></h3>
+        <a href="https://maps.app.goo.gl/DjqWjd8sXgLmm8sd8" target="_blank"> <i class="fa-regular fa-map" style="color: red"></i> {{ $t('Campus on Google maps') }}</a>
       </div>
     </div>
   </div>
